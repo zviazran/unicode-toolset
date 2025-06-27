10d47b500a3307995305409899ea501656dc4dac
Zvi Azran <zviazran@gmail.com>
2025-06-27 18:56:58 +0300

    Ignore Common, Inherited, and Unknown scripts in script coloring logic

--- src/modules/CodepointEditor/ProcessedTextDisplay.tsx
+++ src/modules/CodepointEditor/ProcessedTextDisplay.tsx
@@ -93,7 +93,8 @@ const ProcessedTextDisplay: React.FC<ProcessedTextDisplayProps> = ({ text, setTe
       };
 
       let backgroundColor: string | undefined = undefined;
-      const isStyleTarget = !isInvisible && !isTagChar && !isWordBreakChar && !isNoBreakChar && !isAIIndicator;
+      const shouldIgnoreScript = script === "Common" || script === "Inherited" || script === "Unknown";
+      const isStyleTarget = !isInvisible && !isTagChar && !isWordBreakChar && !isNoBreakChar && !isAIIndicator && !shouldIgnoreScript;
       if (isStyleTarget && !(script in scriptToColor)) {
         const nextIndex = Object.keys(scriptToColor).length;
         scriptToColor[script] = getScriptColor(nextIndex);
