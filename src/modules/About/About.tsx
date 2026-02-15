39e1ed26e5176ef4dd2d75bf72006568eb7cdbc1
zviazran <zviazran@gmail.com>
2026-02-15 16:41:14 +0200

    update About component description for clarity and emphasis on teaching

--- src/modules/About/About.tsx
+++ src/modules/About/About.tsx
@@ -25,7 +25,7 @@ const About: React.FC = () => {
                 </h2>
 
                 <p className="heading-description blockquote">
-                  I'm a teacher and a former software engineer with a decade of experience in JavaScript, C#, C++ and more. Experienced in developing for cybersecurity solutions, optimizing performance, and maintaining complex codebases. Also an unapologetic Unicode enthusiast.<br/><br/>This project is both a showcase of my capabilities, thoroughness and dedication and a love letter to the plain old text that everyone unfairly takes for granted.<br/><br/>My goal is to make this project worthy of your browser favorites 🤗 It's free and publicly available for all. 
+                  I'm a Computer Science & Math teacher and an unapologetic Unicode enthusiast. I'm a former software engineer with a decade of experience developing for cybersecurity solutions, optimizing performance, and maintaining complex codebases.<br/><br/>This project is both a useful toolset for anyone to use or teach with, and a love letter to the plain old text that everyone unfairly takes for granted.<br/><br/>My goal is to make this project worthy of your browser favorites 🤗 It's free and publicly available for all. 
                 </p>
               </Col>
 
